05d79a81bbb30515c27a6f981d31176e4f29be46
OS <OS@192.168.1.219>
2024-08-24 20:11:58 -0500

    Ejercicio sqrt.

---
+++ src/ejercicios/matematicos/EjerciciosMatematicos.java
@@ -0,0 +1,16 @@
+
+package ejercicios.matematicos;
+
+import java.util.Scanner;
+
+public class EjerciciosMatematicos {
+
+    public static void main(String[] args) {
+        double sqrt = Math.sqrt(18);
+        
+        
+        System.out.println(sqrt);
+        
+    }
+    
+}
